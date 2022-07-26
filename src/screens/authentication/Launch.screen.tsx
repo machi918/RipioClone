537d41b0ed37eb01fc66440b2238a01c478a6275
Matias <mati_d_pinto@hotmail.com>
2022-07-26 20:54:39 -0300

    Auth screens added

---
+++ src/screens/authentication/Launch.screen.tsx
@@ -0,0 +1,44 @@
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import React, {FC, useState} from 'react';
+import {View, Text, Image} from 'react-native';
+import {Colors} from '../../assets/theme/Colors';
+import {MainButton} from '../../components/buttons';
+import {AuthNav} from '../../navigation/auth/AuthNav';
+import {BaseScreen} from '../common/baseScreen/BaseScreen.screen';
+
+const LaunchScreen: FC = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<AuthNav>>();
+
+  const [loading, setLoading] = useState<boolean>(false);
+
+  return (
+    <BaseScreen backgroundColor={Colors.white}>
+      <View style={{width: '100%', alignItems: 'center'}}>
+        <Text>V0.00.0</Text>
+        <Image source={require('../../../src/assets/images/ripioLogo.png')} style={{width: 150, height: 80}} />
+      </View>
+      <View>
+        <View style={{width: '100%', height: 350, backgroundColor: 'red', marginBottom: 40}}>
+          <Text>LOGIN</Text>
+        </View>
+
+        <MainButton
+          text="Ingresar"
+          onPress={() => navigation.navigate('LoginScreen')}
+          disabled={loading}
+          marginBottom={10}
+        />
+        <MainButton
+          text="Registrate"
+          textColor={Colors.onSecondary}
+          backgroundColor={Colors.white}
+          onPress={() => navigation.navigate('SignUpScreen')}
+          disabled={loading}
+        />
+      </View>
+    </BaseScreen>
+  );
+};
+
+export default LaunchScreen;
---
+++ src/screens/authentication/signup/SignUp.tsx
@@ -0,0 +1,33 @@
+import React, {FC, useState} from 'react';
+import {View, Text, TextInput, Button} from 'react-native';
+import {BaseScreen} from '../../common/baseScreen/BaseScreen.screen';
+import auth from '@react-native-firebase/auth';
+
+const SignUpScreen: FC = () => {
+  const [mail, setMail] = useState<string>('');
+  const [pass, setPass] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+
+  async function handleSignUp(email: string, password: string) {
+    try {
+      setLoading(true);
+      await auth().createUserWithEmailAndPassword(email, password);
+      console.log('User account created & signed in!');
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <BaseScreen>
+      <View>
+        <Text>SIGN UP</Text>
+        <TextInput placeholder="MAIL" onChangeText={text => setMail(text)} />
+        <TextInput placeholder="PASS" onChangeText={text => setPass(text)} />
+        <Button title="DALE" onPress={() => handleSignUp(mail, pass)} disabled={loading} />
+      </View>
+    </BaseScreen>
+  );
+};
+
+export default SignUpScreen;
