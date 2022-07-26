4cb272521654c21d080986faec9179971f28ea9c
Matias <mati_d_pinto@hotmail.com>
2022-07-26 20:57:57 -0300

    new navigator implemented

--- App.tsx
+++ App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {TabNav} from './src/navigation/TabNav';
 import {store} from './src/redux/store';
 import {Provider} from 'react-redux';
-import {MainNav} from './src/navigation/MainNav';
+import {AppNav} from './src/navigation/app/AppNav';
 
 const App = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        {/* <TabNav /> */}
-        <MainNav />
+        <AppNav />
       </NavigationContainer>
     </Provider>
   );
