beed39ae25bdae3943746dddb48bbcb57fe7de21
Matias <mati_d_pinto@hotmail.com>
2022-07-26 20:55:44 -0300

    fix: buttons atributes & signout enabled

--- src/screens/profile/Profile.screen.tsx
+++ src/screens/profile/Profile.screen.tsx
@@ -3,8 +3,17 @@ import {View, Text, ScrollView} from 'react-native';
 import {Colors} from '../../assets/theme/Colors';
 import {CoinItem, ProfileHeader, ProfileSection, ProfileSectionItem, TitleText} from '../../components';
 import {BaseScreen} from '../index';
+import auth from '@react-native-firebase/auth';
 
 const ProfileScreen: FC = () => {
+  async function handleLogOut() {
+    try {
+      await auth().signOut();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <BaseScreen>
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -13,28 +22,33 @@ const ProfileScreen: FC = () => {
         <ProfileHeader />
 
         <ProfileSection title="Ajustes de perfil">
-          <ProfileSectionItem text="Datos personales" icon="person-outline" />
-          <ProfileSectionItem text="Cambiar contraseña" icon="key-outline" />
-          <ProfileSectionItem text="Cuentas bancarias" icon="cash-outline" />
-          <ProfileSectionItem text="Responsable inscripto" icon="server-outline" />
-          <ProfileSectionItem text="Limites" icon="ellipsis-horizontal-circle-outline" />
+          <ProfileSectionItem text="Datos personales" icon="person-outline" onPress={() => console.log()} />
+          <ProfileSectionItem text="Cambiar contraseña" icon="key-outline" onPress={() => console.log()} />
+          <ProfileSectionItem text="Cuentas bancarias" icon="cash-outline" onPress={() => console.log()} />
+          <ProfileSectionItem text="Responsable inscripto" icon="server-outline" onPress={() => console.log()} />
+          <ProfileSectionItem text="Limites" icon="ellipsis-horizontal-circle-outline" onPress={() => console.log()} />
         </ProfileSection>
 
         <ProfileSection title="Preferencias">
-          <ProfileSectionItem text="Idioma" icon="globe-outline" />
+          <ProfileSectionItem text="Idioma" icon="globe-outline" onPress={() => console.log()} />
         </ProfileSection>
 
         <ProfileSection title="Seguridad">
-          <ProfileSectionItem text="Método de bloqueo" icon="lock-closed-outline" />
+          <ProfileSectionItem text="Método de bloqueo" icon="lock-closed-outline" onPress={() => console.log()} />
         </ProfileSection>
 
         <ProfileSection title="General">
-          <ProfileSectionItem text="Invitar amigos" icon="gift-outline" />
-          <ProfileSectionItem text="Códigos de promoción" icon="pricetags-outline" />
-          <ProfileSectionItem text="Centro de ayuda" icon="information-circle-outline" />
-          <ProfileSectionItem text="Términos y Condiciones" icon="document-outline" />
-          <ProfileSectionItem text="Soporte" icon="chatbox-ellipses-outline" />
-          <ProfileSectionItem text="Cerrar sesión" textColor={Colors.errorRed} icon="log-in-outline" />
+          <ProfileSectionItem text="Invitar amigos" icon="gift-outline" onPress={() => console.log()} />
+          <ProfileSectionItem text="Códigos de promoción" icon="pricetags-outline" onPress={() => console.log()} />
+          <ProfileSectionItem text="Centro de ayuda" icon="information-circle-outline" onPress={() => console.log()} />
+          <ProfileSectionItem text="Términos y Condiciones" icon="document-outline" onPress={() => console.log()} />
+          <ProfileSectionItem text="Soporte" icon="chatbox-ellipses-outline" onPress={() => console.log()} />
+          <ProfileSectionItem
+            text="Cerrar sesión"
+            textColor={Colors.errorRed}
+            icon="log-in-outline"
+            onPress={() => handleLogOut()}
+          />
         </ProfileSection>
 
         <View style={{width: '100%', height: 50, alignItems: 'center'}}>
