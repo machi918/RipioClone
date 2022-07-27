355369311c70786a53c881280dd236552ab3e467
Matias <mati_d_pinto@hotmail.com>
2022-07-26 23:58:46 -0300

    assets added

--- src/assets/dummy/launchData.ts
+++ src/assets/dummy/launchData.ts
@@ -1,27 +1,29 @@
 export const launchData = [
   {
     id: 0,
-    image: 'asdasdasdasd',
+    image: require('../images/launch/1.png'),
     text: 'Descubrí el futuro de la economía digital',
   },
   {
     id: 1,
-    image: 'asdasdasdasd',
+    image: require('../images/launch/2.png'),
     text: 'Tu billetera pone las criptomonedas a tu alcance',
   },
   {
     id: 2,
-    image: 'asdasdasdasd',
+    image: require('../images/launch/3.png'),
     text: 'Enviá y recibí criptomonedas al instante',
   },
   {
     id: 3,
-    image: 'asdasdasdasd',
+
+    image: require('../images/launch/4.jpg'),
     text: 'Comprá y vendé al mejor precio del mercado',
   },
   {
     id: 4,
-    image: 'asdasdasdasd',
+
+    image: require('../images/launch/5.png'),
     text: 'Operá con todas tus criptos desde una única plataforma',
   },
 ];
