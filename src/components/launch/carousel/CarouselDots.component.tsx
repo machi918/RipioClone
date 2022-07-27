3a5631a779c2341bf70dcd89266020abc268a6d3
Matias <mati_d_pinto@hotmail.com>
2022-07-26 23:58:18 -0300

    Carousel created

---
+++ src/components/launch/carousel/CarouselDots.component.tsx
@@ -0,0 +1,27 @@
+import React, {FC} from 'react';
+import {View} from 'react-native';
+import {Colors} from '../../../assets/theme/Colors';
+import {CarouselDot} from './CourselDot.component';
+
+interface CarouselDotsIntertface {
+  pages: number;
+  currentPage: number;
+}
+
+export const CarouselDots: FC<CarouselDotsIntertface> = ({pages, currentPage}) => {
+  const auxData = Array.from({length: pages}, (_, i) => i + 1);
+
+  return (
+    <View
+      style={{
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      {auxData.map(item => (
+        <CarouselDot focused={currentPage === item - 1} key={item - 1} />
+      ))}
+    </View>
+  );
+};
---
+++ src/components/launch/carousel/CourselDot.component.tsx
@@ -0,0 +1,20 @@
+import React, {FC} from 'react';
+import {View} from 'react-native';
+import {Colors} from '../../../assets/theme/Colors';
+
+interface CarouselDotIntertface {
+  focused: boolean;
+}
+
+export const CarouselDot: FC<CarouselDotIntertface> = ({focused}) => {
+  return (
+    <View
+      style={{
+        height: 10,
+        width: 10,
+        marginHorizontal: 5,
+        borderRadius: 5,
+        backgroundColor: focused ? Colors.primary : Colors.secondaryVariant,
+      }}></View>
+  );
+};
