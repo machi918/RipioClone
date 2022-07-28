fa0bf39c41b9d9bc647f666ba8d80679d48502e2
Matias <mati_d_pinto@hotmail.com>
2022-07-28 05:41:49 -0300

    Fetch on load

--- src/navigation/MainNav.tsx
+++ src/navigation/MainNav.tsx
@@ -1,20 +1,40 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {CoinScreen, PayScreen} from '../screens';
+import {CoinScreen, PayScreen, UserInfoScreen} from '../screens';
 import {TabNav} from './TabNav';
 import {CoinScreenInterface} from '../screens/common/coinScreen/Coin.screen';
 import SelectPayScreen, {SelectPayScreenInterface} from '../screens/home/SelectPay.screen';
+import {getAllCoins, getNews} from '../service/https/coins.api';
+import {setGeneralCoins, setNews} from '../redux/coinsSlice';
+import {useAppDispatch} from '../redux/hooks';
 
 export type MainNav = {
   TabNav: undefined;
   CoinScreen: CoinScreenInterface;
   PayScreen: SelectPayScreenInterface;
   SelectPayScreen: undefined;
+  UserInfoScreen: undefined;
 };
 
 const Stack = createNativeStackNavigator<MainNav>();
 
 export const MainNav: FC = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const asd = async () => {
+      try {
+        const response = await getAllCoins();
+        const response2 = await getNews();
+        dispatch(setGeneralCoins(response));
+        dispatch(setNews(response2));
+      } catch (err) {}
+    };
+
+    asd();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="TabNav" component={TabNav} />
@@ -25,6 +45,7 @@ export const MainNav: FC = () => {
         component={SelectPayScreen}
         options={{presentation: 'modal', animation: 'fade_from_bottom'}}
       />
+      <Stack.Screen name="UserInfoScreen" component={UserInfoScreen} />
     </Stack.Navigator>
   );
 };
