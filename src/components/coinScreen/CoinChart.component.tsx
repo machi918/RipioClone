586f1888f5813298d6b46d6ef989b2faccc8581f
Matias <mati_d_pinto@hotmail.com>
2022-07-28 20:00:07 -0300

    Interactive chart added

---
+++ src/components/coinScreen/CoinChart.component.tsx
@@ -0,0 +1,48 @@
+// @ts-nocheck
+import React, {FC, useState} from 'react';
+import {Text} from 'react-native';
+import {Chart, Line, Area, Tooltip} from 'react-native-responsive-linechart';
+import {Colors} from '../../assets/theme/Colors';
+import {CoinHistoricalPriceInterface} from '../../service/https/coins.api';
+import {NewView} from '../common/view/NewView.component';
+
+interface CoinCharInterface {
+  data: CoinHistoricalPriceInterface[];
+}
+
+export const CoinChart: FC<CoinCharInterface> = ({data}) => {
+  const [value, setValue] = useState<CoinHistoricalPriceInterface>({
+    x: data[data.length - 1].x,
+    y: data[data.length - 1].y,
+    date: data[data.length - 1].date,
+  });
+
+  return (
+    <NewView allWidth>
+      <NewView allWidth allCentered>
+        <Text style={{fontSize: 20, color: Colors.black}}>ARS$ {value.y.toFixed(3)}</Text>
+        <Text>{new Date(value.date).toString().slice(0, -24)}</Text>
+      </NewView>
+      <Chart
+        style={{height: '100%', width: '100%'}}
+        padding={{top: 40}}
+        data={data}
+        xDomain={{min: 0, max: data.length}}>
+        <Area theme={{gradient: {from: {color: '#FFFFFF'}, to: {color: '#FFFFFF', opacity: 0.4}}}} />
+        <Line
+          smoothing="none"
+          onTooltipSelect={e => setValue(e)}
+          tooltipComponent={
+            <Tooltip
+              theme={{
+                shape: {width: 4, height: 40, radius: 0},
+                label: {color: 'black', opacity: 0, textAnchor: 'middle'},
+              }}
+            />
+          }
+          theme={{stroke: {color: '#ffa502', width: 5}, scatter: {default: {width: 4, height: 4, rx: 2}}}}
+        />
+      </Chart>
+    </NewView>
+  );
+};
